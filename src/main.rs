0a610858f00978eebe0e7af081b398251fdccde9
NeedPainkillers <46500324+NeedPainkillers@users.noreply.github.com>
2020-02-29 13:58:55 +0300

    UPD: write to file

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use gio::prelude::*;
 use glib::clone;
 use gtk::prelude::*;
 use gtk::{IconSize, Orientation, ReliefStyle, Widget, Entry, Box, Button, Application, ApplicationWindow};
-
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let application = Application::new(
@@ -29,8 +30,13 @@ fn main() {
 
         entry.connect_activate(|x| println!("{}",x.get_text().unwrap()));
         button.connect_clicked(move |_| {
-            println!("Clicked!");
-            println!("{}", entry.get_text().unwrap());
+            let text = entry.get_text().unwrap();
+            println!("{}", text);
+            let mut file = File::create("data");
+            let mut writer = std::io::BufWriter::new(file.unwrap());
+            writer.write_all(text.as_bytes());
+            //writer.write_all(&[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+            //file.write_all(text.as_bytes());
         });
 
 
