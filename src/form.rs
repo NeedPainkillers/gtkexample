74e6a0f7d993a7f95767c5b2767772b7ef62ce5f
NeedPainkillers <46500324+NeedPainkillers@users.noreply.github.com>
2020-03-23 21:07:46 +0300

    UPD: write

--- src/form.rs
+++ src/form.rs
@@ -32,12 +32,12 @@ pub fn build_ui(application: &gtk::Application) {
     let window: gtk::Window = builder.get_object("window1").expect("Couldn't get window");
     window.set_application(Some(application));
 
-    let app = app::new(window.clone());
+    let mut app = Rc::new(RefCell::new(app::new(window.clone())));
 
     //this item used to storage entry fields in order to add it to resulting List of students
-    let mut student = Rc::new(RefCell::new(Student {id: "".to_string(), name: "".to_string(), age: 0u8}));
-    let name_entry: Entry = builder.get_object("entry1").expect("Couldn't get name entry field");
-    let id_entry: Entry = builder.get_object("entry3").expect("Couldn't get id entry field");
+    let mut student = Rc::new(RefCell::new(Student {id: "".to_string(), name: "".to_string(), age: 0u8, _id_internal: -1i32}));
+    let name_entry: Entry = builder.get_object("entry3").expect("Couldn't get id entry field");
+    let id_entry: Entry = builder.get_object("entry1").expect("Couldn't get name entry field");
     let age_entry: Entry = builder.get_object("entry2").expect("Couldn't get age entry field");
 
     name_entry.connect_changed(clone!(@strong student, @weak name_entry => move |_|
@@ -63,7 +63,8 @@ pub fn build_ui(application: &gtk::Application) {
 
         }));
 
-    let open_button: gtk::MenuItem = builder.get_object("imagemenuitem12").expect("Couldn't get open button from menu bar");
+    let open_button: gtk::MenuItem = builder.get_object("imagemenuitem12").expect("Couldn't get read button from menu bar");
+    let write_button: gtk::MenuItem =  builder.get_object("imagemenuitem14").expect("Couldn't get write button from menu bar");
 
     let paned: gtk::Paned = builder.get_object("paned1").expect("Couldn't get paned");
     //let grid: gtk::Grid = builder.get_object("grid1").expect("Couldn't get grid");
@@ -72,44 +73,75 @@ pub fn build_ui(application: &gtk::Application) {
 
     let add_button: gtk::Button = builder.get_object("button1").expect("Couldn't get add button");
     let mut i = Rc::new(Cell::new(1));
-    add_button.connect_clicked(clone!(@weak window, @strong i, @strong student => move |_| {
+    add_button.connect_clicked(clone!(@weak window, @strong i, @strong student, @strong app => move |_| {
     //TODO: read from libs binded to entry fields and add result to LinkedList which will be written to file
         let grid: gtk::Grid = builder.get_object("grid1").expect("Couldn't get grid");
         let entry1 = Entry::new();
         let entry2 = Entry::new();
         let entry3 = Entry::new();
+        let delete_button = gtk::Button::new();
 
-        let item =
+        let mut item =
         {
             let borrowed = student.as_ref();
             entry1.set_text(borrowed.borrow().borrow().id.as_str());
             entry2.set_text(borrowed.borrow().borrow().name.as_str());
             entry3.set_text(borrowed.borrow().borrow().age.to_string().as_str());
-            Rc::new(RefCell::new(Student {id: borrowed.borrow().borrow().id.as_str().to_string(), name: borrowed.borrow().borrow().name.as_str().to_string(), age: borrowed.borrow().borrow().age}))
+            //RcRefStudent::new(Student {id: borrowed.borrow().borrow().id.as_str().to_string(), name: borrowed.borrow().borrow().name.as_str().to_string(), age: borrowed.borrow().borrow().age})
+            Rc::new(RefCell::new(Student {
+              id: borrowed.borrow().borrow().id.as_str().to_string(),
+              name: borrowed.borrow().borrow().name.as_str().to_string(),
+              age: borrowed.borrow().borrow().age,
+              _id_internal: -1i32})) //TODO: save current id somewhere
         };
+        //TODO: check this
+        app.borrow_mut().storage.add(item.clone());
 
         entry1.connect_changed(clone!(@strong item, @weak entry1 => move |_|
         {
             item.borrow_mut().id = entry1.get_text().expect("Couldn't get text from id entry").to_string();
         }));
+        entry2.connect_changed(clone!(@strong item, @weak entry2 => move |_|
+        {
+            item.borrow_mut().name = entry2.get_text().expect("Couldn't get text from id entry").to_string();
+        }));
+        entry2.connect_changed(clone!(@strong item, @weak entry3 => move |_|
+        {
+             if entry3.get_text().expect("Couldn't get text from age entry").to_string().trim().is_empty()
+             {
+                return;
+             }
+             let value = entry3.get_text().expect("Couldn't get text from age entry").to_string().trim().parse();
+             match value {
+             Ok(x) => item.borrow_mut().age = x,
+             Err(e) => entry3.set_text(format!("ERROR INPUT {:?}", e).as_str())
+             }
+        }));
+        delete_button.connect_clicked(clone!(@strong item, @weak entry1, @weak entry2, @weak entry3,
+         @weak delete_button, @weak app, @weak grid => move |_|
+        {
+        //TODO: saving id internal locally to avoid unnecessary dereferencing
+             app.borrow_mut().storage.delete(item.as_ref().borrow().borrow()._id_internal);
+             grid.remove(&entry1);
+             grid.remove(&entry2);
+             grid.remove(&entry3);
+             grid.remove(&delete_button);
+        }));
 
         grid.attach(&entry1, 0, (*i).get() /*row*/, 1, 1);
         grid.attach_next_to(&entry2, Some(&entry1),gtk::PositionType::Right,1,1);
         grid.attach_next_to(&entry3, Some(&entry2),gtk::PositionType::Right,1,1);
+        grid.attach_next_to(&delete_button, Some(&entry3),gtk::PositionType::Right,1,1);
         (*i).set((*i).get()+1);
 
-        let mut linked_list = StudentList::new();
-        let student1 = Student {id: "0".to_string(), name: "smth".to_string(), age: 18};
-        let student2 = Student {id: "1".to_string(), name: "smb".to_string(), age: 19};
-        linked_list.add(student1);
-        linked_list.add(student2);
-        linked_list.write_to_file(&"test".to_string());
+
+        //app.borrow_mut().storage.write_to_file(&"test".to_string());
 
         window.show_all();
     }));
 
 
-
+    //reader
     open_button.connect_activate(clone!(@weak window => move |_| {
         let file_chooser = gtk::FileChooserDialog::new(
             Some("Open File"),
@@ -127,11 +159,32 @@ pub fn build_ui(application: &gtk::Application) {
             let mut reader = BufReader::new(file);
             let mut contents = String::new();
             let _ = reader.read_to_string(&mut contents);
-
+            //TODO: parse content and show it on screen
             //text_view
             //    .get_buffer()
             //    .expect("Couldn't get window")
             //    .set_text(&contents);
+        }
+
+        file_chooser.destroy();
+    }));
+
+    write_button.connect_activate(clone!(@weak window, @weak app => move |_| {
+        let file_chooser = gtk::FileChooserDialog::new(
+            Some("Save File"),
+            Some(&window),
+            gtk::FileChooserAction::Save,
+        );
+        file_chooser.add_buttons(&[
+            ("Save", gtk::ResponseType::Ok),
+            ("Cancel", gtk::ResponseType::Cancel),
+        ]);
+        if file_chooser.run() == gtk::ResponseType::Ok {
+            let filename = file_chooser.get_filename().expect("Couldn't get filename");
+            //let file = File::open(&filename).expect("Couldn't open file");
+
+            app.borrow_mut().storage.write_to_file_choosen(filename.as_path());
+            println!("Write success");
         }
 
         file_chooser.destroy();
--- src/libs/model/student.rs
+++ src/libs/model/student.rs
@@ -3,17 +3,24 @@ use std::collections::LinkedList;
 use std::io::BufWriter;
 use std::io::prelude::*;
 use std::fmt;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 use itertools::Itertools;
+use itertools::rciter;
 
 use crate::libs::app::app::app;
+use std::borrow::{BorrowMut, Borrow};
+use std::fs::File;
+
 
 #[derive(Clone)]
 pub struct Student
 {
     pub id: String,
     pub name: String,
-    pub age: u8
+    pub age: u8,
+    pub _id_internal: i32
 }
 
 impl fmt::Display for Student {
@@ -22,10 +29,78 @@ impl fmt::Display for Student {
     }
 }
 
+
+#[derive(Clone)]
+pub struct RefStudent
+{
+    pub item: RefCell<Student>
+}
+
+impl fmt::Display for RefStudent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Student: id: \"{0}\", name: \"{1}\", age: {2};", self.item.borrow().id, self.item.borrow().name, self.item.borrow().age as char)
+    }
+}
+
+impl From<RefCell<Student>> for RefStudent {
+    fn from(item: RefCell<Student>) -> Self {
+        RefStudent {item: item.clone()}
+    }
+}
+
+impl RefStudent
+{
+    pub fn new(item: Student) -> RefStudent
+    {
+        RefStudent {item: RefCell::new(item)}
+    }
+}
+
+
+pub struct RcRefStudent
+{
+    pub item: Rc<RefStudent>
+}
+
+impl Clone for RcRefStudent
+{
+    fn clone(&self) -> RcRefStudent
+    {
+        RcRefStudent {item: self.item.clone()}
+    }
+}
+
+impl From<Rc<RefStudent>> for RcRefStudent
+{
+    fn from(item: Rc<RefStudent>) -> Self {
+        RcRefStudent {item: item.clone()}
+    }
+}
+
+impl fmt::Display for RcRefStudent {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.item.item.borrow())
+    }
+}
+
+impl RcRefStudent
+{
+    pub fn new(item: Student) -> RcRefStudent
+    {
+        RcRefStudent {item: Rc::new(RefStudent::new(item))}
+    }
+
+    pub fn borrow_mut(&mut self) -> std::cell::RefMut<Student>
+    {
+        return self.item.item.borrow_mut();
+    }
+}
+
+
 #[derive(Clone)]
 pub struct StudentList
 {
-    storage: LinkedList<Student>
+    storage: LinkedList<Rc<RefCell<Student>>>
 }
 
 impl StudentList
@@ -38,7 +113,7 @@ impl StudentList
             }
     }
 
-    pub fn add(&mut self, item: Student) -> &mut StudentList
+    pub fn add(&mut self, item: Rc<RefCell<Student>>) -> &mut StudentList
     {
         self.storage.push_back(item);
         return self;
@@ -47,6 +122,8 @@ impl StudentList
     pub fn delete(&mut self, index: i32) -> &mut StudentList
     {
         //TODO: pop until index is equal to given, then push back popped vales except deleted one
+        self.storage = rciter(self.storage.clone()).filter(|x|
+            RefCell::borrow(x.as_ref())._id_internal != index ).collect();
         return self;
     }
 
@@ -54,8 +131,32 @@ impl StudentList
     {
         let mut file = std::fs::File::create(path).expect(format!("{0}{1}{2}","Cannot create file at '", path, "'").as_str());
         let mut writer = BufWriter::new(file);
-        let text = self.storage.iter().join("\n");
+
+        //TODO:iter
+        let mut text = String::new();
+        for i in self.storage.clone()
+            {
+                //println!("{}", i.as_ref().borrow());
+                text.push_str(format!("{}\n",i.as_ref().borrow()).as_str());
+            }
         writer.write_all(text.as_bytes()).expect((format!("{0}{1}{2}", "Cannot write to file '", path, "'").as_str()));
+
+        return self;
+    }
+    pub fn write_to_file_choosen(&mut self, path: &std::path::Path) -> &mut StudentList
+    {
+        let mut file = std::fs::File::create(path).expect(format!("{0}{1}{2}","Cannot create file at '", (*path).display(), "'").as_str());
+        let mut writer = BufWriter::new(file);
+
+        //TODO:iter
+        let mut text = String::new();
+        for i in self.storage.clone()
+            {
+                //println!("{}", i.as_ref().borrow());
+                text.push_str(format!("{}\n",i.as_ref().borrow()).as_str());
+            }
+        writer.write_all(text.as_bytes()).expect((format!("{0}", "Cannot write to file").as_str()));
+
         return self;
     }
 }
